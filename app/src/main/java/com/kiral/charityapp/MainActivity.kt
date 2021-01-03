818ad1a85253824bd5c912e3571302816b2d906b
Ivan Kiral <kiralivan@gmail.com>
2021-01-03 18:56:45 +0100

    Create Project

---
+++ app/src/main/java/com/kiral/charityapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kiral.charityapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
