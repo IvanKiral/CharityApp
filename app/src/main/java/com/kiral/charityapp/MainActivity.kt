cd65c79c74e0b01154555a546c9cfca76353b38b
Ivan Kiral <kiralivan@gmail.com>
2021-05-05 18:59:24 +0200

    added notifications

--- app/src/main/java/com/kiral/charityapp/MainActivity.kt
+++ app/src/main/java/com/kiral/charityapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.kiral.charityapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
+import com.kiral.charityapp.notifications.createNotificationChannel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        createNotificationChannel(this,
+            NotificationManagerCompat.IMPORTANCE_DEFAULT, false,
+            getString(R.string.app_name), "App notification channel"
+        )
     }
 }
